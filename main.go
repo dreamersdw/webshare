6dfc76fbd903d52ba08bd4a4cc6cc15e2cf098e2
dreamersdw <dreamersdw@gmail.com>
2015-03-14 23:23:17 +0800

    Serve file through http protocol

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/op/go-logging"
@@ -23,4 +25,13 @@ func setupLogging() {
 func main() {
 	setupLogging()
 	log.Info("start webshare ...")
+	dir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	http.Handle("/fs/", http.StripPrefix("/fs/", http.FileServer(http.Dir(dir))))
+	http.ListenAndServe("0.0.0.0:8888", nil)
 }
